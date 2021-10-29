1b5ff52c67041554c46351d6e2ba4fde18f387b0
Jason Dinsmore <jason@dinjas.com>
2021-10-29 10:36:34 -0700

    Use letters for loggers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,8 +5,8 @@ require 'logger'
 
 Knapsack::Adapters::RSpecAdapter.bind
 
-LOGGER1 = Logger.new("log/test1-#{ENV.fetch('BUILDKITE_PARALLEL_JOB', 0)}.log")
-LOGGER2 = Logger.new("log/test2-#{ENV.fetch('BUILDKITE_PARALLEL_JOB', 0)}.log")
+LOGGER1 = Logger.new("log/testA-#{ENV.fetch('BUILDKITE_PARALLEL_JOB', 0)}.log")
+LOGGER2 = Logger.new("log/testB-#{ENV.fetch('BUILDKITE_PARALLEL_JOB', 0)}.log")
 LOGGER1.formatter = -> (_severity, _datetime, _progname, msg) do
   "#{msg.to_json}\n"
 end
